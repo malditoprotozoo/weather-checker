3c283a7630016460dc33c2eacd1c3984830086ac
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-03-14 00:29:51 -0300

    adding content to temperature component

---
+++ src/components/weathercontainer/temperature/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Typography from 'material-ui/Typography';
+
+class Temperature extends Component {
+  render() {
+    return (
+      <Typography>
+        30º
+      </Typography>
+    );
+  }
+}
+
+export default Temperature;
